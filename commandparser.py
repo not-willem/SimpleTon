df20d6769a74134c715563c951930e24f8237171
not-willem <vanhuwi26034@whs.school.nz>
2026-08-21 17:16:28 +1200

    added some more commands

--- commandparser.py
+++ commandparser.py
@@ -1,5 +1,5 @@
 import linecache
-
+import re
 
 # creating a new storage, you probably shouldnt do this every time but it works for little tests
 
@@ -9,7 +9,8 @@ with open("STORAGE.SV", "w") as file:
 
 #command parser
 def parse(command, line, verbose):
-    commandlist = command.split()
+    #split
+    commandlist = re.findall(r'"[^"]*"|\S+', command)
     parsecommmand = commandlist[0]
     if parsecommmand == "m":
             try:
@@ -42,6 +43,18 @@ def parse(command, line, verbose):
                         number = int(number)
                         #put the string in the sector
                         mov(string + "\n", number-1)
+                elif findtype(commandlist[1]) == "number":
+                    #if type 1 is a numbrero
+                    numbrero = commandlist[1].replace("(", "")
+                    numbrero = numbrero.replace(")", "")
+                    if findtype(commandlist[2]) == "sector":
+                        # oh no he's onto me i think he knows im a sector oh no
+                        number = commandlist[2].replace("x", "")
+                        if verbose:
+                            print(numbrero)
+                        number = int(number)
+                        # put
+                        mov(str(numbrero) + "\n", number-1)
             except:
                 if line == 0:
                     print("Please check your spelling!")
@@ -49,17 +62,53 @@ def parse(command, line, verbose):
                     print(f"Please check your spelling on line {line}")
 
     elif parsecommmand == "p":
-        print('print')
+        if findtype(commandlist[1]) == "string":
+            print(commandlist[1].replace('"',""))
+        elif findtype(commandlist[1]) == "number":
+            fixed = commandlist[1].replace(')',"")
+            fixed = fixed.replace('(',"")
+            print(fixed)
+        elif findtype(commandlist[1]) == "sector":
+            if verbose:
+                print(commandlist[1].replace('x',""))
+            print(getline(int(commandlist[1].replace('x',""))).strip())
     elif parsecommmand == "a":
-        print('add')
+        if findtype(commandlist[1]) == "number":
+            if findtype(commandlist[2]) == "sector":
+                numbrero = int(getline(int(commandlist[2].replace("x", ""))))
+                fixed = commandlist[1].replace("(", "")
+                numbrero = numbrero + int(fixed.replace(")", ""))
+                mov(str(numbrero), int(commandlist[2].replace("x", ""))-1)
+        else:
+            if line == 0:
+                print("Please enter a number!")
+            else:
+                print(f"Please enter a number on line {line}")
     elif parsecommmand == "s":
-        print('subtract')
+        if findtype(commandlist[1]) == "number":
+            if findtype(commandlist[2]) == "sector":
+                numbrero = int(getline(int(commandlist[2].replace("x", ""))))
+                fixed = commandlist[1].replace("(", "")
+                numbrero = numbrero - int(fixed.replace(")", ""))
+                mov(str(numbrero), int(commandlist[2].replace("x", ""))-1)
+        else:
+            if line == 0:
+                print("Please enter a number!")
+            else:
+                print(f"Please enter a number on line {line}")
     elif parsecommmand == "c":
         print("call")
     elif parsecommmand == "j":
         print("jump")
     elif parsecommmand == "i":
         print("if")
+    elif parsecommmand == "f":
+        if line == 0:
+            print("You cannot create functions in shell mode!")
+        else:
+            print("i should implement functions..")
+    elif parsecommmand == "#":
+        pass
     else:
         if not line == 0:
             print(f'Failed to parse command "{parsecommmand}" on line {line}.. Maybe check your spelling?')
@@ -74,6 +123,10 @@ def mov(text, line):
     with open("STORAGE.SV", "w") as file:
         file.writelines(lines)
 
+def getline(line):
+    linecache.clearcache()
+    return linecache.getline("STORAGE.SV", line)
+
 def findtype(argument):
     if "x" in argument:
         return "sector"
@@ -81,4 +134,5 @@ def findtype(argument):
         return "string"
     if "(" in argument:
         return "number"
-    
+    else:
+        return "error"
